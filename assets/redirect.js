3bb0cd981d672648aeaefb18264f4a5f6304c9eb
Alouis02 <163452952+Alouis02@users.noreply.github.com>
2025-04-15 15:15:17 -0400

    Delete assets/redirect.js

--- assets/redirect.js
+++
@@ -1,7 +0,0 @@
-setTimeout(function() {
-    window.location.href = "https://alouis02.github.io/AtomicGames/home.html";
-}, 5000); // 5 seconds
-
-git add _config.yml
-git commit -m "Force rebuild"
-git push
